2c8b9d2c2be9c521243bc522e25e7ec210d4605b
GLetiwa <graceletiwa@gmail.com>
2024-06-15 10:32:36 -0700

    Updates

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,15 +2,15 @@ import RedisClient from '../utils/redis';
 import MongoClient from '../utils/db';
 
 class AppController {
-    static getStatus(req, res) {
-        res.status(200).json({ redis: RedisClient.isAlive(), db: MongoClient.isAlive() });
-    }
+  static getStatus(req, res) {
+    res.status(200).json({ redis: RedisClient.isAlive(), db: MongoClient.isAlive() });
+  }
 
-    static async getStats(req, res) {
-        const users = await MongoClient.nbUsers();
-        const files = await MongoClient.nbFiles();
-        res.status(200).json({ users, files });
-    }
+  static async getStats(req, res) {
+    const users = await MongoClient.nbUsers();
+    const files = await MongoClient.nbFiles();
+    res.status(200).json({ users, files });
+  }
 }
 
 export default AppController;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// server.js
 import express from 'express';
 import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
