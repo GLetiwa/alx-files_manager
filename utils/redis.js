ba44f94047ab8edb573b3520dfb3025cc756f06a
GLetiwa <graceletiwa@gmail.com>
2024-06-16 03:18:33 -0700

    New updates

--- utils/redis.js
+++ utils/redis.js
@@ -1,35 +1,47 @@
-// utils/redis.js
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => console.error('Redis client error:', err));
 
-    this.client.connect()
-      .then(() => console.log('Redis client connected'))
-      .catch((err) => console.error('Redis client connection error:', err));
-    
+    this.client.on('error', (err) => {
+      console.error('Redis error:', err);
+    });
+
+    this.client.on('connect', () => {
+      console.log('Redis client connected');
+    });
+
+    // Promisify Redis commands
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.setAsync = promisify(this.client.set).bind(this.client);
     this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
-  isAlive() {
+  async connect() {
+    if (!this.client.isOpen) {
+      await this.client.connect();
+    }
+  }
+
+  async isAlive() {
+    await this.connect();
     return this.client.isOpen;
   }
 
   async get(key) {
+    await this.connect();
     return this.getAsync(key);
   }
 
   async set(key, value, duration) {
-    await this.setAsync(key, value);
-    await this.client.expire(key, duration);
+    await this.connect();
+    await this.setAsync(key, value, 'EX', duration);
   }
 
   async del(key) {
+    await this.connect();
     return this.delAsync(key);
   }
 }
